dc0cd0b212b9003677c24c30eb9cf6af63d54ed3
badr59k <khelifi.badr.1@gmail.com>
2025-11-09 10:57:06 +0100

    feat(auth): ajoute RegisterDTO, /health, entity User et schema users; config JDBC

---
+++ auth-service/src/main/java/com/example/authservice/entity/User.java
@@ -0,0 +1,59 @@
+package com.example.authservice.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    //Attributs
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    @Column(name = "password_hash")
+    private String passwordHash;
+
+    private LocalDateTime createdAt;
+
+
+    //constructeur vide pour JPA
+    public User() {}
+
+    // Accesseurs
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
